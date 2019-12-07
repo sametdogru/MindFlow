ca0ba57d91febdb7543e216b7ddcfe1c4ed1e238
smttdgru <smttdgru1907@gmail.com>
2019-12-07 13:34:00 +0300

    test

--- BisuTestApp/Login/ViewController.swift
+++ BisuTestApp/Login/ViewController.swift
@@ -1,9 +1,9 @@
 import UIKit
-
 class ViewController: UIViewController {
 
 	@IBOutlet weak var signUpButton: UIButton!
 	@IBOutlet weak var signInButton: UIButton!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
